56f0bab1416221133ce4a663adaadf5a7cc0e880
João Victor <jvoliveira2005@gmail.com>
2023-05-19 22:25:46 -0300

    mudança de fundo terminada

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let weatherTempElement = document.querySelector("#weather-temp")
 let humidityElement = document.querySelector("#humidity")
 let windElement = document.querySelector("#wind")
 let weatherDescriptionElement = document.querySelector("#weather-description")
-
+let celsiusTermometerElement = document.querySelector(".celsius-termometer")
 let backgroundElement = document.querySelector(".background-container")
+
+let weatherIconElement = document.querySelector(".weather-icon")
 
 
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
@@ -21,21 +23,53 @@ const getWeatherData = async (city) => {
 	return data;
   };
 
+const changeElements = (cityName, weatherTemp, celsiusTerm, humidity, wind,
+						weatherDescription,backgroundImg, backgroundColor)=>{
+	cityNameElement.style.color = cityName;
+	weatherTempElement.style.color = weatherTemp;
+	celsiusTermometerElement.style.color = celsiusTerm;
+	humidityElement.style.color = humidity;
+	windElement.style.color = wind;
+	weatherDescriptionElement.style.color = weatherDescription;
+	backgroundElement.style.backgroundImage = backgroundImg;
+	backgroundElement.style.backgroundColor = backgroundColor;
+					
+}
   
 const changeIcon = async(city)=>{
 
-	//let weatherDescription = "Thunderstorm"
 	let weatherDescription = city.weather[0].main
 
 
 	if(weatherDescription == "Clouds"){
-		backgroundElement.style.backgroundColor = "rgb(228,228,228)";
+		changeElements("black", "black", "black", "black", "black", "black", "none", "rgb(192,181,169)")
 	}
 	else if(weatherDescription == "Clear"){
-		backgroundElement.style.backgroundColor = "rgb(184,230,240)";
+		changeElements("black", "black", "black", "black", "black", "black", "none", "rgb(184,230,240)")
 	}
-	else if(weatherDescription == "Thunderstorm"){
-		backgroundElement.style.backgroundColor = "rgb(109,109,109)";
+	else if(weatherDescription == "Thunderstorm"  || weatherDescription == "Squall"){
+		changeElements("white", "white", "white", "#70757a", "#70757a", "#70757a", "url(./style/src/thunderstorm-gif.gif", "none")
+	}
+	else if(weatherDescription == "Drizzle"){
+		changeElements("white", "white", "white", "#70757a", "#70757a", "#70757a", "url(./style/src/drizzle-gif.gif", "none")
+	}
+	else if (weatherDescription == "Rain"){
+		changeElements("white", "white", "white", "#70757a", "#70757a", "#70757a", "url(./style/src/rain-gif.gif", "none")
+	}
+	else if (weatherDescription == "Snow"){
+		changeElements("black", "black", "black", "black", "black", "black", "url(./style/src/snow-gif.gif", "none")
+	}
+	else if (weatherDescription == "Tornado"){
+		changeElements("black", "black", "black", "black", "black", "black", "url(./style/src/tornado-gif.gif", "none")
+	}
+	else if (weatherDescription == "Mist" || weatherDescription == "Smoke" || weatherDescription == "Haze" || weatherDescription == "Fog"){
+		changeElements("black", "black", "black", "black", "black", "black", "url(./style/src/mist-gif.gif", "none")
+	}
+	else if (weatherDescription == "Dust" || weatherDescription == "Sand"){
+		changeElements("black", "black", "black", "black", "black", "black","none", "rgb(234,162,091)")
+	}
+	else if (weatherDescription == "Ash"){
+		changeElements("black", "black", "black", "black", "black", "black","url(./style/src/volcanic-ash-gif.gif)", "none")
 	}
 
 }
@@ -49,9 +83,11 @@ const showWeatherData = async (city)=>{
 	humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
 	windElement.textContent = `Wind: ${data.wind.speed}km/h`;
 	weatherDescriptionElement.textContent = (data.weather[0].description).charAt(0).toUpperCase() + (data.weather[0].description).slice(1);
+	weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+
 
 	changeIcon(data)
 }
 
 
-showWeatherData("Moscow")
+showWeatherData("Kiowa")
